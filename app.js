f2715c854de5ab52565537911962812d4340b087
Allison Alexander <aveleber@hotmailcom>
2017-03-11 12:48:55 -0500

    working web page

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 const express = require('express');
-const volleyball = require('volleyball');
-const chalk = require('chalk');
-
 //creates an instance of an express application
 const app = express();
+const volleyball = require('volleyball');
+const chalk = require('chalk');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+const path = require('path');
 
+// templating boilerplate setup for nunjucks
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+app.set('view engine', 'html'); // have res.render work with html files
+nunjucks.configure('views', {noCashe: true}); // point nunjucks to the proper directory for templates
 
-
-
-
-
+//loggint middleware
 app.use(volleyball);
-//simple route to root page
-app.get('/', function(req, res) {
-	res.send('Welcome to my web page!!!');
-});
 
-
-
-
-//at bottom of page
+//start the server
 //tell app to listen for requests
 app.listen(3113, function(){
 	console.log(chalk.magenta('Listening on port 3113!!'));
 });
+
+
+//static routing
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+//setup modular routing
+app.use('/', routes);
+
+
